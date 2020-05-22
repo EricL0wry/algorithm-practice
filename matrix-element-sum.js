7258134b9dbc9681a2b841cb524b0cf356054eb8
Eric Lowry <ericmichaellowry@gmail.com>
2020-05-22 10:02:55 -0700

    Set up file for matrix elements sum

---
+++ matrix-element-sum.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+
+// After becoming famous, the CodeBots decided to move into a new building
+// together.Each of the rooms has a different cost, and some of them are free, but
+// there's a rumour that all the free rooms are haunted! Since the CodeBots are
+// quite superstitious, they refuse to stay in any of the free rooms, or any of
+// the rooms below any of the free rooms.
+
+// Given matrix, a rectangular matrix of integers, where each value represents the
+// cost of the room, your task is to return the total sum of all rooms that are
+// suitable for the CodeBots(ie: add up all the values that don't appear
+// below a 0).
+
+// special considerations: matrix length of 1, matrix made up of arrays with length of 1
+
+const matrix1 = [[0, 1, 1, 2], [0, 5, 0, 0], [2, 0, 3, 3]]; /* expected output: 9 */
+
+function matrixElementsSum(matrix) {
+  const roomTotal = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+
+    }
+  }
+}
