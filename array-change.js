d42094301ce7e6fab70febb2b07ade4264c6ad9a
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-08 09:32:28 -0700

    Completed pseudo code

--- array-change.js
+++ array-change.js
@@ -9,6 +9,21 @@
 // For inputArray = [1, 1, 1], the output should be
 // arrayChange(inputArray) = 3.
 
+// Pseudo Code
+
+// Create variable to hold moveCount
+// Iterate through the array starting at i = 1
+//  If array[i] === to array[i-1]
+//    Add one to array[i]
+//    Increment moveCount by one
+//  If array[i] > array[i-1]
+//    Continue
+//  If array[i] < array[i-1]
+//    Create a variable to hold difference of array[i-1] - array[i]
+//    Add one to this value for the amount needed to make strictly increasing
+//    Add total value to array[i]
+//    Add total value to moveCount
+
 function arrayChange(inputArray) {
 
 }
