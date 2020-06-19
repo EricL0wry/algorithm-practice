4ae0af30493a3b95e5f23265e81158959c442119
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-19 14:28:05 -0700

    Completed Even Digits Only problem from CodeSignal

--- arcade/even-digits-only.js
+++ arcade/even-digits-only.js
@@ -9,6 +9,27 @@
 // For n = 642386, the output should be
 // evenDigitsOnly(n) = false.
 
-function evenDigitsOnly(n) {
+// Pseudo Code
 
+// We need to iterate through each digit of the integer given
+// Numbers do not have a .length property, and cannot be iterated through like a string
+
+// Convert number to a string using Number.prototype.toString()
+// Iterate through each character using a for loop
+// If the remainder of the character divided by 2 is truthy, return false
+// Return true
+
+// Test n = 248622, expected result true
+
+function evenDigitsOnly(n) {
+  const numberString = n.toString(10); // number = '248622'
+  for (let i = 0; i < numberString.length; i++) { // i = 3, length = 6
+    const number = +numberString[i];
+    if (number % 2) { // number[2] = '8' % 2 = 8 % 2 = 0
+      return false;
+    }
+  }
+  return true;
 }
+
+console.log(evenDigitsOnly(642886));
