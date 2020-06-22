fe13b3a9da02d3fa8c1e1b0522b0e02edae59154
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-22 08:46:23 -0700

    Initial problem description and function skeleton setup

---
+++ arcade/chess-board-cell-color.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+
+// Given two cells on the standard chess board, determine whether they have the
+// same color or not.
+
+// For cell1 = "A1" and cell2 = "C3", the output should be
+// chessBoardCellColor(cell1, cell2) = true.
+
+// For cell1 = "A1" and cell2 = "H3", the output should be
+// chessBoardCellColor(cell1, cell2) = false.
+
+function chessBoardCellColor(cell1, cell2) {
+
+}
