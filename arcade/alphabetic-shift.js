bac0f6cba9a77060966d03b269566e9e631049be
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-22 08:13:20 -0700

    Added problem description, example, and function skeleton

---
+++ arcade/alphabetic-shift.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-console */
+
+// Given a string, your task is to replace each of its characters by the next one
+// in the English alphabet; i.e.replace a with b, replace b with c, etc(z would be
+// replaced by a).
+
+// Example
+
+// For inputString = "crazy", the output should be alphabeticShift(inputString) = "dsbaz".
+
+function alphabeticShift(inputString) {
+
+}
