d1be84c10d50870d1487c7ba8cebb47354caad07
Eric Lowry <ericmichaellowry@gmail.com>
2020-07-06 17:16:23 -0700

    Added problem description, function skeleton, and notes

---
+++ arcade/extract-each-kth.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-console */
+
+// Given array of integers, remove each kth element from it.
+
+// Example
+
+// For inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] and k = 3, the output should be
+// extractEachKth(inputArray, k) = [1, 2, 4, 5, 7, 8, 10].
+
+// Notes
+
+// create a variable holding an empty return array
+// Iterate through input array
+// If not a kth element, push to return array
+// If it is a kth element ((i+1) % k = 0), do not push to return array
+// Return array
+
+// Pseudo code
+
+function extractEachKth(inputArray, k) {
+
+}
