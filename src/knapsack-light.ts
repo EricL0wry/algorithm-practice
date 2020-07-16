086eae8f5c9a7576c6329bf6777ae6f580f33ea6
Eric Lowry <ericmichaellowry@gmail.com>
2020-07-15 17:37:58 -0700

    Completed initial code, but failing a hidden test. Need to refactor

---
+++ src/knapsack-light.ts
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+
+// You found two items in a treasure chest! The first item weighs weight1 and is
+// worth value1, and the second item weighs weight2 and is worth value2. What is
+// the total maximum value of the items you can take with you, assuming that your
+// max weight capacity is maxW and you can't come back for the items later?
+
+// Note that there are only two items and you can't bring more than one item of
+// each type, i.e. you can't take two first items or two second items.
+
+function knapsackLight(
+  value1: number, // 15
+  weight1: number, // 2
+  value2: number, // 20
+  weight2: number, // 3
+  maxW: number // 2
+) {
+  if (weight1 + weight2 <= maxW) return value1 + value2;
+  if (weight1 > maxW && weight2 > maxW) return 0;
+  if (weight1 > weight2 && weight1 <= maxW)
+    return value1 > value2 ? value1 : value2;
+  if (weight2 > weight1 && weight2 <= maxW)
+    return value2 > value1 ? value2 : value1;
+  if (weight1 < weight2 && weight2 > maxW) return value1;
+  if (weight2 < weight1 && weight1 > maxW) return value2;
+}
