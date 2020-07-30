f14daf6a06243c052d1fb9f712020b8e4885af84
Eric Lowry <ericmichaellowry@gmail.com>
2020-07-29 17:54:22 -0700

    Completed bishop and pawn problem from CodeSignal

--- src/bishop-and-pawn.ts
+++ src/bishop-and-pawn.ts
@@ -15,4 +15,34 @@
  *
  */
 
-function bishopAndPawn(bishop, pawn) {}
+interface Axis {
+  [key: string]: number;
+}
+
+function bishopAndPawn(bishop: string, pawn: string): boolean {
+  const xAxis: Axis = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+  };
+  const pawnX = pawn[0];
+  const pawnY = pawn[1];
+  const bishopX = bishop[0];
+  const bishopY = bishop[1];
+
+  if (
+    Math.abs(xAxis[pawnX] - xAxis[bishopX]) ===
+    Math.abs(Number(pawnY) - Number(bishopY))
+  ) {
+    return true;
+  }
+
+  return false;
+}
+
+console.log(bishopAndPawn('h1', 'h3'));
