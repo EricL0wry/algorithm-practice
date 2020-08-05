bdf3fae9b09a29f6d28c86830408b01d16e4fbe1
Eric Lowry <ericmichaellowry@gmail.com>
2020-08-05 16:39:39 -0700

    Initial file setup for Build Palindrome problem from codesignal

---
+++ arcade/build-palindrome.js
@@ -0,0 +1,19 @@
+"use strict";
+/* eslint-disable no-console */
+/*
+ *
+ * Given a string, find the shortest possible string which can be achieved by adding characters to the end of initial 3
+ * string to make it a palindrome.
+ *
+ * Example
+ *
+ * For st = "abcdc", the output should be
+ * buildPalindrome(st) = "abcdcba".
+ *
+ */
+function buildPalindrome(st) {
+    return 'word';
+}
+function checkPalindrome(st) {
+    return true;
+}
---
+++ src/build-palindrome.ts
@@ -0,0 +1,21 @@
+/* eslint-disable no-console */
+
+/*
+ *
+ * Given a string, find the shortest possible string which can be achieved by adding characters to the end of initial 3
+ * string to make it a palindrome.
+ *
+ * Example
+ *
+ * For st = "abcdc", the output should be
+ * buildPalindrome(st) = "abcdcba".
+ *
+ */
+
+function buildPalindrome(st: string): string {
+  return 'word';
+}
+
+function checkPalindrome(st: string): boolean {
+  return true;
+}
