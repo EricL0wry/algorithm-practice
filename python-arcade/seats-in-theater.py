4e5eec918be1173d8eaf98b5aa8a01c8040a2694
Eric Lowry <ericmichaellowry@gmail.com>
2020-10-19 17:06:37 -0700

    Completed seats-in-theater problem from codesignal

--- python-arcade/seats-in-theater.py
+++ python-arcade/seats-in-theater.py
@@ -16,3 +16,8 @@
 
 # For nCols = 16, nRows = 11, col = 5, and row = 3, the output should be
 # seatsInTheater(nCols, nRows, col, row) = 96.
+
+def seatsInTheater(nCols, nRows, col, row):
+  return (nCols - col + 1) * (nRows - row)
+
+print(seatsInTheater(16,11,5,3))
