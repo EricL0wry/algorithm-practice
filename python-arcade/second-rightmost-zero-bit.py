82e4d5cc660bd76353cf8053f29a22aaa277d943
Eric Lowry <ericmichaellowry@gmail.com>
2020-11-13 16:42:55 -0800

    Completed second-rightmost-zero-bit problem from codesignal

--- python-arcade/second-rightmost-zero-bit.py
+++ python-arcade/second-rightmost-zero-bit.py
@@ -8,11 +8,11 @@
 # For n = 37, the output should be
 # secondRightmostZeroBit(n) = 8.
 
-# 3710 = 1001012. The second rightmost zero bit is at position 3 (0-based) from the right in the binary representation 
+# 37 = 100101. The second rightmost zero bit is at position 3 (0-based) from the right in the binary representation 
 # of n.
 # Thus, the answer is 23 = 8.
 
 def secondRightmostZeroBit(n):
-    return ...
+    return 2**int(bin(n)[::-1].replace('0', '2', 1).find('0'))
 
 print(secondRightmostZeroBit(37))
