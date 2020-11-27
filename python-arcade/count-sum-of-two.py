771942e49702d139ec58a53368a86f6b87892963
Eric Lowry <ericmichaellowry@gmail.com>
2020-11-27 10:01:02 -0800

    Working code, but fails some time execution tests

--- python-arcade/count-sum-of-two.py
+++ python-arcade/count-sum-of-two.py
@@ -9,6 +9,15 @@
 # There are just two ways to write 6 as A + B, where 2 ≤ A ≤ B ≤ 4: 6 = 2 + 4 and 6 = 3 + 3.
 
 def countSumOfTwoRepresentations2(n, l, r):
-  return ...
+  sum_count = 0
+
+  for num_a in range(l, r+1):
+    for num_b in range(r, l-1, -1):
+      if num_a > num_b:
+        break
+      if num_a + num_b == n:
+        sum_count += 1
+
+  return sum_count
 
 print(countSumOfTwoRepresentations2(6, 2, 4))
