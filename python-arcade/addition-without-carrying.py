54b51453d61d45d7f40d6660c0be970d7b3b7021
Eric Lowry <ericmichaellowry@gmail.com>
2020-11-30 16:33:30 -0800

    Completed addition without carrying problem from codesignal

--- python-arcade/addition-without-carrying.py
+++ python-arcade/addition-without-carrying.py
@@ -1,4 +1,4 @@
-#  little boy is studying arithmetic. He has just learned how to add two integers, written one below another, column 
+#  A little boy is studying arithmetic. He has just learned how to add two integers, written one below another, column 
 #  by column. But he always forgets about the important part - carrying.
 
 # Given two integers, your task is to find the result which the little boy will get.
@@ -24,6 +24,21 @@
 # is written before 456. Thus, he gets 0 + 1 = 1.
 
 def additionWithoutCarrying(param1, param2):
-  return
+  param1 = str(param1)[::-1]
+  param2 = str(param2)[::-1]
+  param1Len = len(param1) - 1
+  param2Len = len(param2) - 1
+  longLen = param1Len if param1Len >= param2Len else param2Len
+  returnNum = ''
+  
+
+  for num in range(0, longLen + 1):
+    if param1Len >= num and param2Len >= num:
+      returnNum = str(int(param1[num]) + int(param2[num]))[-1] + returnNum
+    elif param1Len >= num:
+      returnNum = param1[num] + returnNum
+    else:
+      returnNum = param2[num] + returnNum
+  return int(returnNum)
 
 print(additionWithoutCarrying(456, 1734))
