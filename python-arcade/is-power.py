b0638b0f2beffdacea33ab228b6069f4aee2ba98
Eric Lowry <ericmichaellowry@gmail.com>
2020-12-29 16:13:54 -0800

    Completed is-power problem from Codesignal

--- python-arcade/is-power.py
+++ python-arcade/is-power.py
@@ -8,15 +8,16 @@
 # isPower(n) = false.
 
 def isPower(n):
-  for num in range(1, n+1):
+  if n == 1:
+    return True
+  for num in range(2, n):
     result = 0
     power = 2
-    print(num)
-    while result < n:
+    while result <= n:
       result = num ** power
       power += 1
       if result == n:
         return True
   return False
 
-print(isPower(125))
+print(isPower(400))
