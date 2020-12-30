0bdcc13ac4b0bf3bb5ce1f15f30e254754085a94
Eric Lowry <ericmichaellowry@gmail.com>
2020-12-30 09:46:54 -0800

    Working code, but failing one test on time constraints

---
+++ python-arcade/is-sum-of-consecutive-2.py
@@ -0,0 +1,28 @@
+# Find the number of ways to express n as sum of some (at least two) consecutive positive integers.
+
+# Example
+
+# For n = 9, the output should be
+# isSumOfConsecutive2(n) = 2.
+
+# There are two ways to represent n = 9: 2 + 3 + 4 = 9 and 4 + 5 = 9.
+
+# For n = 8, the output should be
+# isSumOfConsecutive2(n) = 0.
+
+# There are no ways to represent n = 8.
+
+def isSumOfConsecutive2(n):
+  ways = 0
+  for num in range(1, n):
+    total = num
+    consecutive = 1
+    for num2 in range(num + 1, n):
+      total += num2
+      consecutive += 1
+      if total == n and consecutive > 1:
+        ways += 1
+        break
+  return ways
+
+print(isSumOfConsecutive2(8))
