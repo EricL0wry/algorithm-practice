bb9da0b4eef6635cfd522e29c90427991069a726
Eric Lowry <ericmichaellowry@gmail.com>
2021-01-30 15:33:50 -0800

    continued work on rectangle rotation

--- python-arcade/rectangle-rotation.py
+++ python-arcade/rectangle-rotation.py
@@ -9,23 +9,28 @@ def rectangleRotation(a, b):
   # First find points where center square crosses axes
   # Then calculate the 4 corners of the diagonal rectangle
     # The sides of the center square are equal to the smaller of the two values
-  # Then expand that to a larger rectangle that covers the diagonal rectangle. Basically, a search area
-  # Starting at the upper left of this rectangle, scan each column
-    # Calculate the borders of the diagonal rectangle for that column
-    # For each integer point in each column within or on those bounds, increment a running total
+  # Start at left corner
+    # First column to check for points is equal to int(x) which will remove any decimal
   totalPoints = 0
   long = max(a, b)
   short = min(a, b)
   axisIntersection = math.sqrt((short / 2)** 2 * 2)
   rise = math.sqrt(.5 ** 2 * 2)
-  pointA = (-(axisIntersection + (((long - short) / 2) * rise)), -(0 + (((long - short) / 2) * rise)))
-  pointB = (0 + (((long - short) / 2) * rise), axisIntersection + (((long - short) / 2) * rise))
-  pointC = (axisIntersection + (((long - short) / 2) * rise), 0 + (((long - short) / 2) * rise))
-  pointD = (-(0 + (((long - short) / 2) * rise)), -(axisIntersection + (((long - short) / 2) * rise)))
-  # totalPoints += int(intersection) * 4 + 1
+  left = (-(axisIntersection + (((long - short) / 2) * rise)), -(0 + (((long - short) / 2) * rise)))
+  top = (0 + (((long - short) / 2) * rise), axisIntersection + (((long - short) / 2) * rise))
+  right = (axisIntersection + (((long - short) / 2) * rise), 0 + (((long - short) / 2) * rise))
+  bottom = (-(0 + (((long - short) / 2) * rise)), -(axisIntersection + (((long - short) / 2) * rise)))
+  start = left
+
+  if start == (int(start[0]), int(start[1])):
+    totalPoints += 1
+  for x_axis_point in range(int(left[0]), int(right[0] + 1)):
+    print(x_axis_point)
+  
+
   
 
 
-  return pointD
+  return (1, 2) == (1, 2)
 
 print(rectangleRotation(6, 4))
